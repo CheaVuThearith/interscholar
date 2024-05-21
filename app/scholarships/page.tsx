988f2eaf8d103a9eb3ad633e91830df2fc04d2a4
CheaVuThearith <cheavuthearith0@gmail.com>
2024-05-21 12:04:56 +0700

    some paddings

--- app/scholarships/page.tsx
+++ app/scholarships/page.tsx
@@ -30,14 +30,14 @@ const page = ({ searchParams }: Props) => {
       <div>
         <div className="mx-auto mt-20 flex w-[90%] flex-col items-center justify-center gap-y-20 rounded-xl bg-white p-0 py-20 pb-10 shadow-lg">
           <div
-            className="grid w-full gap-12"
+            className="grid p-3 w-full gap-12"
             style={{
               gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
             }}
           >
             {shownItems.map((entry, index) => (
               <EventCard
-                className="max-w-[368px] transform cursor-pointer self-start justify-self-center rounded-xl p-0 lg:p-6 transition-all duration-200 hover:scale-105 hover:border hover:shadow-xl"
+                className="max-w-[368px] transform cursor-pointer self-start justify-self-center rounded-xl p-0 lg:p-6 transition-all duration-200 lg:hover:scale-105 lg:hover:border lg:hover:shadow-xl"
                 key={index}
                 name={entry}
               />

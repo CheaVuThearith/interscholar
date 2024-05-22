82b1dd7615c98553074e7f3c22cf4d56b115ee2c
CheaVuThearith <cheavuthearith0@gmail.com>
2024-05-22 19:18:34 +0700

    integrated database

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -13,7 +13,9 @@ const Navbar = (props: Props) => {
     <>
       <div className="sticky z-10 inset-x-10 top-0 flex h-24 overflow-hidden items-center justify-center gap-6 bg-[#ffffff] p-8 drop-shadow-lg xl:justify-between">
         <div className="size-24 after:bottom-5 after:left-2 relative bottom-2 after:content-['InterScholar'] after:size-20 after:font-semibold after:text-sm after:relative after: after:text-[#fa9183]">
+          <a href="/">
           <img src="interscholarlogo.webp" alt="logo" />
+          </a>
         </div>
 
         <div className="absolute -z-10 inset-x-0 hidden items-center justify-center gap-x-12 xl:flex">

008b00a677145cd000cb4405249d79330b3dea99
CheaVuThearith <cheavuthearith0@gmail.com>
2024-05-23 01:40:46 +0700

    added pics

--- app/_components/FeatureCard.tsx
+++ app/_components/FeatureCard.tsx
@@ -12,7 +12,6 @@ const Card = ({ title, children }: Props) => {
       className='after:delay-500 after:content-["Loremsasdasfdhqashfbjakjsbfkajsgfdiqhwgrjfhasfhasgfgaeyudfgfqasuyihfgauhsdghasdfgasjhdasdagsrhf"] after:absolute overflow-visible after:top-[300px] after:w-max after:rounded-lg after:duration-200 after:opacity-0 hover:after:opacity-100 after:transition-all after:shadow-lg after:p-5 after:bg-white after:shadow-[#000000a6] after:h-36 after:text-black relative flex size-64 flex-col rounded-xl bg-[#e1f0da]'
     >
       <div className="flex h-[75%] w-full items-center justify-center overflow-hidden">
-        {" "}
         {children}
       </div>
       <p className="flex grow items-center justify-center bg-[#ffffff90] text-2xl font-semibold text-[#515050]">

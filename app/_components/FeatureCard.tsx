b6f46d55a220d01d6b3decc211e7206e338f8332
CheaVuThearith <cheavuthearith0@gmail.com>
2024-05-22 21:59:28 +0700

    changed some file names

--- app/_components/FeatureCard.tsx
+++ app/_components/FeatureCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Card = ({ title, children }: Props) => {
   return (
     <div
-      className='after:content-["Loremsasdasfdhqashfbjakjsbfkajsgfdiqhwgrjfhasfhasgfgaeyudfgfqasuyihfgauhsdghasdfgasjhdasdagsrhf"] after:absolute overflow-visible after:top-[300px] after:w-max after:rounded-lg after:duration-200 after:opacity-0 hover:after:opacity-100 after:transition-all after:shadow-lg after:p-5 after:bg-white after:shadow-[#000000a6] after:h-36 after:text-black relative flex size-64 flex-col rounded-xl bg-[#e1f0da]'
+      className='after:delay-500 after:content-["Loremsasdasfdhqashfbjakjsbfkajsgfdiqhwgrjfhasfhasgfgaeyudfgfqasuyihfgauhsdghasdfgasjhdasdagsrhf"] after:absolute overflow-visible after:top-[300px] after:w-max after:rounded-lg after:duration-200 after:opacity-0 hover:after:opacity-100 after:transition-all after:shadow-lg after:p-5 after:bg-white after:shadow-[#000000a6] after:h-36 after:text-black relative flex size-64 flex-col rounded-xl bg-[#e1f0da]'
     >
       <div className="flex h-[75%] w-full items-center justify-center overflow-hidden">
         {" "}

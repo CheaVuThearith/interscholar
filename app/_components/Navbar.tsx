b18dd3492b888e47bab2a4321980235128fef6ce
CheaVuThearith <cheavuthearith0@gmail.com>
2024-05-28 22:24:34 +0700

    changes to navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,9 +1,10 @@
 "use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 import SearchBar from "./SearchBar";
-import Image from "next/image";
-import { useEffect, useRef } from "react";
 type Props = {};
 
 const Navbar = (props: Props) => {
@@ -16,76 +17,91 @@ const Navbar = (props: Props) => {
     "Internships",
   ];
 
-  let scrollY = 0;
   const NavbarRef = useRef<HTMLDivElement>(null);
 
-  const handleScroll = () => {
-    console.log(window.scrollY);
-    if (NavbarRef.current) {
-      if (
-        window.scrollY > 16 &&
-        !NavbarRef.current.classList.contains("nav_background_on")
-      ) {
-        NavbarRef.current.classList.add("nav_background_on");
-      } else if (window.scrollY < 16) {
-        NavbarRef.current.classList.remove("nav_background_on");
-      }
-    }
-  };
   useEffect(() => {
+    const handleScroll = () => {
+      if (NavbarRef.current) {
+        if (window.scrollY > 16 && navOpen) {
+          setNavOpen(false);
+          setColorOn(true);
+        } else if (window.scrollY < 16) {
+          setColorOn(false);
+          setNavOpen(true);
+        }
+      }
+    };
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.addEventListener("scroll", handleScroll);
     };
   }, [NavbarRef]);
+
+  const [navOpen, setNavOpen] = useState(true);
+  const [colorOn, setColorOn] = useState(false);
   return (
     <>
-      <nav
-        ref={NavbarRef}
-        className="sticky inset-x-10 top-0 z-10 mt-4 flex h-20 items-center justify-center gap-6 overflow-hidden px-8 drop-shadow-lg transition-all duration-200 xl:justify-between"
-      >
-        <div className="flex h-20 items-center justify-center">
-          <a
-            href="/"
-            className="relative flex items-center justify-center gap-x-2"
-          >
-            <Image
-              width={60}
-              height={60}
-              src="/interscholarlogo.png"
-              alt="logo"
-            />
-            <p className="flex items-center justify-center text-xl font-semibold text-[#85bd71]">
-              InterScholar
-            </p>
-          </a>
-        </div>
-
-        <div className="hidden items-center justify-center gap-x-12 xl:flex">
-          {pages.map((page, index) => {
-            const href = `/opportunities/${page.toLowerCase().replace("-", "")}`;
-            return (
-              <Link
-                key={index}
-                onMouseEnter={() => {
-                  router.prefetch(href);
-                }}
-                className="text-lg"
-                href={href}
-              >
-                {page}
-              </Link>
-            );
-          })}
-        </div>
-
-        <div className="hidden items-center justify-center gap-x-5 xl:flex">
-          <SearchBar />
-          <button className="hidden rounded-md bg-[#99bc85] px-3 py-2 xl:block">
-            Login
-          </button>
-        </div>
-      </nav>
+        <motion.nav
+          onMouseEnter={() => setNavOpen(true)}
+          ref={NavbarRef}
+          className={`sticky top-5 z-10 mt-4 flex xl:ml-8 m-auto h-24 items-center justify-center gap-6 overflow-hidden rounded-full ${navOpen ? "p-8 xl:justify-between": "xl:justify-center"} ${colorOn && "bg-[#faf6ef]"} drop-shadow-lg `}
+          animate={
+            !navOpen
+              ? { width: 96}
+              : {
+                  width: "97%",
+                }
+          }
+          transition={{
+            default: {duration:.2 },
+          }}
+        >
+          <div className={`flex h-20 items-center justify-center`}>
+            <a
+              href="/"
+              className="flex items-center justify-center gap-x-2 transition-all"
+            >
+              <Image
+                width={60}
+                height={60}
+                src="/interscholarlogo.png"
+                alt="logo"
+              />
+              {navOpen && (
+                <p className="flex items-center justify-center text-xl font-semibold text-[#85bd71]">
+                  InterScholar
+                </p>
+              )}
+            </a>
+          </div>
+          {navOpen && (
+            <>
+              <div className="hidden items-center justify-center gap-x-12 xl:flex">
+                {pages.map((page, index) => {
+                  const href = `/opportunities/${page.toLowerCase().replace("-", "")}`;
+                  return (
+                    <Link
+                      key={index}
+                      onMouseEnter={() => {
+                        router.prefetch(href);
+                      }}
+                      className="text-lg"
+                      href={href}
+                    >
+                      {page}
+                    </Link>
+                  );
+                })}
+              </div>
+              <div className="hidden items-center justify-center gap-x-5 xl:flex">
+                <SearchBar />
+                <button className="hidden rounded-md bg-[#99bc85] px-3 py-2 xl:block">
+                  Login
+                </button>
+              </div>
+            </>
+          )}
+        </motion.nav>
     </>
   );
 };

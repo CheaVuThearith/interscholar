91480fb4b406dce492824e651c38d31cd50dc205
CheaVuThearith <cheavuthearith0@gmail.com>
2024-05-29 10:42:42 +0700

    formatting

--- app/_components/FilterMenu.tsx
+++ app/_components/FilterMenu.tsx
@@ -9,9 +9,9 @@ const FilterMenu = ({ filterOptions }: Props) => {
   const [activeFilters, setActiveFilters] = useState<string[]>([]);
   const [filterOn, setFilterOn] = useState(false);
   return (
-    <div className="flex h-full lg:sticky top-40 lg:w-[300px] w-full shrink-0 grow-0 flex-col items-center justify-start lg:items-start gap-y-10">
+    <div className="top-40 flex h-full w-full shrink-0 grow-0 flex-col items-center justify-start gap-y-10 lg:sticky lg:w-[300px] lg:items-start">
       <button
-        className={`size-16 rounded-full p-3 transition-all duration-200 ${filterOn && "shadow-xl bg-[#00000020]"} `}
+        className={`size-16 rounded-full p-3 transition-all duration-200 ${filterOn && "bg-[#00000020] shadow-xl"} `}
         onClick={() => {
           setFilterOn((f) => !f);
         }}
@@ -19,7 +19,7 @@ const FilterMenu = ({ filterOptions }: Props) => {
         <AdjustmentsHorizontalIcon />
       </button>
       {filterOn && (
-        <ul className="flex flex-row lg:flex-col w-full overflow-x-scroll lg:w-auto gap-y-1">
+        <ul className="flex w-full flex-row gap-y-1 overflow-x-scroll lg:w-auto lg:flex-col">
           {filterOptions.map((filter, index) => (
             <li
               onClick={() => {
@@ -31,7 +31,7 @@ const FilterMenu = ({ filterOptions }: Props) => {
                   );
                 }
               }}
-              className="rounded-xl cursor-pointer px-4 py-2 transition-all duration-200 hover:bg-[#00000023]"
+              className="cursor-pointer rounded-xl px-4 py-2 transition-all duration-200 hover:bg-[#00000023]"
               key={index}
             >
               {filter}
